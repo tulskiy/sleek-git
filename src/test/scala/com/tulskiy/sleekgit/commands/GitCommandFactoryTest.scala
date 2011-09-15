94d2a96a10359fcc2617c497145c2960cfa2db3b
Denis Tulskiy <tulskiy@gmail.com>
2011-09-15 15:29:30 +0600

    [tests] fixed command factory test

--- src/test/scala/com/tulskiy/sleekgit/commands/GitCommandFactoryTest.scala
+++ src/test/scala/com/tulskiy/sleekgit/commands/GitCommandFactoryTest.scala
@@ -8,7 +8,7 @@ import org.specs2.mutable._
  */
 
 class GitCommandFactoryTest extends Specification {
-  /*"The GitUploadPackRE" should {
+  "The GitUploadPackRE" should {
     "match good input" in {
       "git-upload-pack 'test.git'" must matchUploadWithPath("test.git")
       "git upload-pack '/test.git'" must matchUploadWithPath("test.git")
@@ -29,8 +29,6 @@ class GitCommandFactoryTest extends Specification {
       "git-upload-pack '~user/test.git/./wdr'" must not(matchUpload)
       "git-upload-pack '~user/test.git/../wdr'" must not(matchUpload)
     }
-
-
   }
 
   def matchUpload = beMatching(GitCommandFactory.GitUploadPackRE.toString())
@@ -38,16 +36,4 @@ class GitCommandFactoryTest extends Specification {
 
   def matchUploadWithPath(path: String) = find(GitCommandFactory.GitUploadPackRE.toString()).withGroup(path)
   def matchReceiveWithPath(path: String) = find(GitCommandFactory.GitReceivePackRE.toString()).withGroup(path)
-*/
-  "The 'Hello world' string" should {
-        "contain 11 characters" in {
-          "Hello world" must have size(11)
-        }
-        "start with 'Hello'" in {
-          "Hello world" must startWith("Hello")
-        }
-        "end with 'world'" in {
-          "Hello world" must endWith("world")
-        }
-      }
 }
