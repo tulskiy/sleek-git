b239ad9bcfd9a043f41d46b7ccef9ad4b0ae03f5
Denis Tulskiy <tulskiy@gmail.com>
2011-10-04 17:31:15 +0600

    ignore my test file and some other stuff

--- src/main/scala/com/tulskiy/sleekgit/Test.scala
+++ src/main/scala/com/tulskiy/sleekgit/Test.scala
@@ -1,7 +1,13 @@
 package com.tulskiy.sleekgit
 
+import commands.{UploadPackCommand, GitCommandFactory}
 import java.util.Scanner
+import org.eclipse.jgit.lib.RepositoryBuilder
+import java.io.File
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.revwalk.RevCommit
+
+import scala.collection.JavaConversions._
 
 /**
  * Author: Denis Tulskiy
@@ -9,15 +15,32 @@ import org.eclipse.jgit.api.Git
  */
 object Test {
   def main(args: Array[String]) {
-//    val builder = new ProcessBuilder("cmd", "/c", "dir");
-    val builder = new ProcessBuilder("c:\\Programs\\Git\\bin\\bash.exe", "-c", "\"git gui\"");
+//    val builder = new ProcessBuilder("git", "upload-pack", "tesafdgt.git");
+////    val builder = new ProcessBuilder("c:\\Programs\\Git\\bin\\bash.exe", "-c", "\"git gui\"");
+//
+//    val process = builder.start
+//    val is = process.getErrorStream
+//
+//    val scanner = new Scanner(is)
+//    while (scanner.hasNextLine) {
+//      println(scanner.nextLine())
+//    }
 
-    val process = builder.start
-    val is = process.getInputStream
 
-    val scanner = new Scanner(is)
-    while (scanner.hasNextLine) {
-      println(scanner.nextLine())
-    }
+//    "st" match {
+//      case "wed" => println("")
+//    }
+//
+    val repository = new GitCommandFactory().buildRepository("repo/musique.git")
+
+    val command = new UploadPackCommand(repository)
+
+    command.setInputStream(System.in)
+    command.setOutputStream(System.out)
+    command.setErrorStream(System.err)
+
+    command.start(null)
+
+
   }
 }
